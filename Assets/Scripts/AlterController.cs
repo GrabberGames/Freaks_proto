e8784079d85e154e73e7ec262f809246039a823c
Seung-Ki Kim <kimsk200012@gmail.com>
2021-03-15 21:57:09 +0900

    Prototype_20210315_wlvlrtm
    1) 영웅, 알터 조작 기능 업데이트
    
    2) 스크립트 파일 세분화
    - GameController: 영웅, 알터 클릭 감지
    - AlterController: 알터 조작 제어
    - HeroController: 영웅 조작 제어
    
    // TODO
    1) NavMeshAgent > AgentType 2개 이상 생성 시, "SetDestination" can only be called on an active agent that has been placed on a NavMesh. 에러 발생
    
    2) AlterController, HeroController 내 중복 코드 최적화 필요

--- Assets/Scripts/AlterController.cs
+++ Assets/Scripts/AlterController.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class HeroController : MonoBehaviour
+public class AlterController : MonoBehaviour
 {
-    private NavMeshAgent hero;
-    private RaycastHit hit; // Hit Checker
+    private NavMeshAgent alter;
+    private RaycastHit hit;    // Hit Checker
 
     private bool isClicked;
 
     GameController gameController;
 
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
-        hero = GetComponent<NavMeshAgent>();
+        alter = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        if (gameController.FindPC() == "Hero")  // if Clicked PC name is Hero
+        if(gameController.FindPC() == "Alter")  // if Clicked PC name is Alter
         {
             isClicked = true;   // Clicked Check
         }
@@ -31,14 +31,15 @@ public class HeroController : MonoBehaviour
             isClicked = false;
         }
 
-        if (Input.GetMouseButtonDown(1) && isClicked == true)    // Right Mouse Click && Hero Clicked
+        if (Input.GetMouseButtonDown(1) && isClicked == true) // Right Mouse Click && Alter Clicked
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    // Ray Set; Mouse Pointer Position
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                hero.SetDestination(hit.point); // Hero Move
+                alter.SetDestination(hit.point);    // Alter Move
             }
-        } 
+        }
+
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameController : MonoBehaviour
+{
+    private NavMeshAgent pc;
+    private RaycastHit hit;    // Hit Checker
+    private string hitColliderName;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        pc = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        FindPC();
+    }
+
+
+    // Find + return Cliked PC name
+    public string FindPC()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left Mouse Click
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit, Mathf.Infinity);   // Shot Ray
+
+            hitColliderName = hit.collider.name;   
+        }
+
+        return hitColliderName;  // return Clicked PC Name
+    }
+
+}
